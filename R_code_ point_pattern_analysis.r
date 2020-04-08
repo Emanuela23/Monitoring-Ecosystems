0f504f5fe4cccbf13a028d65731a5dd0ced9b95f
Emanuela23 <62661711+Emanuela23@users.noreply.github.com>
2020-04-08 17:11:41 +0200

    Update R_code_ point_pattern_analysis.r

--- R_code_ point_pattern_analysis.r
+++ R_code_ point_pattern_analysis.r
@@ -20,3 +20,63 @@ d <- density(covids)
 plot(d)
 points(covids)
 
+#----
+
+setwd("C:/lab/")
+load("spatial.RData")
+ls()
+
+# covids: point pattern 
+# d: density map
+library(spatstat)
+
+plot(d)
+points(covids)
+
+# let's see where the points are located - let's add the coastlines 
+
+install.packages("rgdal")
+library("rgdal") 
+
+# let's input vector lines (x0y0, x1y1, x2,y2..)
+coastlines <- readOGR("ne_10m_coastline.shp")
+
+# let's add the lines to the previous image
+plot(coastlines, add=T)
+
+# change the color and make the map beautiful 
+
+cl <- colorRampPalette(c("yellow", "orange", "red")) (100)
+# cl <- colorRampPalette(c('yellow', 'orange', 'red'))(100) #
+# 100: all colors from yellow to red (red for the hotspots)
+
+plot(d, col=cl) # cl: is the colorRampPalette
+points(covids)
+plot(coastlines, add=T) 
+
+# Exercise: new colorRampPalette
+
+cl <- colorRampPalette(c("blue","green","yellow")) (100)
+plot(d, col=cl, main="Densities of covid-19") 
+points(covids)
+plot(coastlines, add=T)
+
+# example of prof: 
+# clr <- colorRampPalette(c("light green", "yellow", "orange", "violet")) (100)
+
+# export
+pdf("covid_density.pdf")
+cl <- colorRampPalette(c("blue","green","yellow")) (100)
+plot(d, col=cl, main="Densities of covid-19") 
+points(covids)
+plot(coastlines, add=T)
+dev.off()
+
+# export png
+png("covid_density.png")
+clr <- colorRampPalette(c("light green", "yellow","orange","violet")) (100)
+plot(d, col=clr, main="Densities of covid-19")
+points(covids)
+plot(coastlines, add=T)
+dev.off()
+
