737a406be869f17b44432e6053a868feec08bad8
Emanuela23 <62661711+Emanuela23@users.noreply.github.com>
2020-05-20 18:23:36 +0200

    Update R_code_faPAR.r

--- R_code_faPAR.r
+++ R_code_faPAR.r
@@ -52,6 +52,48 @@ faPAR10 <- raster("C:/lab/faPAR10.tif")
 levelplot(faPAR10) 
 dev.off
 
+########################################################################
 
+# regression model between faPAR and NDVI
+erosion <- c(12, 14, 16, 24, 26, 40, 55, 67) #amount in kg of erosion 
+hm <- c(30, 100, 150, 200, 260, 340, 460, 600) #amount of heavy metals in ppm
+
+plot(erosion, hm, col="red", pch=19, xlab="erosion", ylab="heavy metals")
+
+model1 <- lm(hm ~ erosion)
+summary(model1)
+abline(model1)
+
+#######
+setwd("C:/lab/")
+library(raster)
+library(rasterdiv)
+faPAR10 <- raster("faPAR10.tif")
+
+plot(faPAR10)
+plot(copNDVI)
+
+copNDVI <- reclassify(copNDVI, cbind(253:255, NA), right=TRUE)
+
+faPAR10 
+
+
+library(sf) # to call st_* functions
+random.points <- function(x,n)
+{
+lin <- rasterToContour(is.na(x))
+pol <- as(st_union(st_polygonize(st_as_sf(lin))), 'Spatial') # st_union to dissolve geometries
+pts <- spsample(pol[1,], n, type = 'random')
+}
+
+plot(faPAR10)
+pts <- random.points(faPAR10,1000)
+copNDVIp <- extract(copNDVI, pts)
+faPAR10p <- extract(faPAR10, pts)
+
+# photosynthesis vs. biomass
+model2 <- lm(faPAR10p ~ copNDVIp)
+plot(copNDVIp, faPAR10p, col="green", xlab="biomass", ylab="photosynthesis")
+abline(model2, col="red")
 
 
