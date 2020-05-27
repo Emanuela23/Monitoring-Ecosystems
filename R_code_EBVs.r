b51a2075de0f6599eabe61e88bb71007c4bf41cc
Emanuela23 <62661711+Emanuela23@users.noreply.github.com>
2020-05-27 17:28:13 +0200

    Update R_code_EBVs.r

--- R_code_EBVs.r
+++ R_code_EBVs.r
@@ -44,5 +44,48 @@ par(mfrow=c(1,2))
 plotRGB(snt, 4, 3, 2, stretch="Lin", main= "original image")
 plot(sd_snt, col=cl, main="diversity")
 
+################################# day 2 Cladonia example
 
+setwd("C:/lab/")
+library(raster)
+
+clad <- brick("cladonia_stellaris_calaita.JPG")
+
+plotRGB(clad, 1,2,3, stretch="lin")
+ 
+window <- matrix(1, nrow = 3, ncol = 3)
+window
+
+### PCA analysis 
+cladpca <- rasterPCA(clad)
+
+cladpca
+
+summary(cladpca$model)
+# 98% Comp 1 (because it's a visible spectrum)
+
+ plotRGB(cladpca$map, 1, 2, 3, stretch="lin")
+
+# set the moving window 
+
+sd_clad <- focal(cladpca$map$PC1, w=window, fun=sd)
+
+PC1_agg <- aggregate(cladpca$map$PC1, fact=10)
+sd_clad_agg <- focal(PC1_agg, w=window, fun=sd)
+
+par(mfrow=c(1,2))
+cl <- colorRampPalette(c('yellow','violet','black'))(100) #
+plot(sd_clad, col=cl)
+plot(sd_clad_agg, col=cl)
+# graph left: original cladonia set - graph righ: aggregated set 
+
+# plot the calculation 
+par(mfrow=c(1,2))
+cl <- colorRampPalette(c('yellow','violet','black'))(100) #
+plotRGB(clad, 1,2,3, stretch="lin")
+plot(sd_clad, col=cl)
+# plot(sd_clad_agg, col=cl)
+
+
+ 
 
