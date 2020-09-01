8f8337fc3985abfeab6429449d7434374d2818b0
Emanuela23 <62661711+Emanuela23@users.noreply.github.com>
2020-09-01 11:12:53 +0200

    Update R_code_first.r

--- R_code_first.r
+++ R_code_first.r
@@ -1,7 +1,7 @@
-install.packages("sp")
+install.packages("sp") #Classes and methods for spatial data; the classes document where the spatial location information resides, for 2D or 3D data. Utility functions are provided, e.g. for plotting data as maps, spatial selection, as well as methods for retrieving coordinates, for subsetting, print, summary, etc.
 
-library(sp)
-data(meuse)
+library(sp) #loads the package
+data(meuse) #loads specified data sets, or list the available data sets.
 
 # Let's see how the meuse dataset is structured:
 meuse
@@ -11,8 +11,8 @@ head(meuse)
 
 # let's plot two variables
 # let's see if the zin concentration is related to that of copper
-attach(meuse) 
+attach(meuse) #the database is attached to the R search path. This means that the database is searched by R when evaluating a variable, so objects in the database can be accessed by simply giving their names.
 plot(zinc,copper)
 plot(zinc,copper,col="green")
-plot(zinc,copper,col="green",pch=19)
-plot(zinc,copper,col="green",pch=19,cex=2)
+plot(zinc,copper,col="green",pch=19) #pch: different point symbols
+plot(zinc,copper,col="green",pch=19,cex=2) #cex: dimension of points
