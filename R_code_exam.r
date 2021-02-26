a0dac191a73c2f833a5f4b00d0735d1314d18648
Emanuela Cosma <62661711+EmanuelaCosma@users.noreply.github.com>
2021-02-26 02:38:20 +0100

    Update R_code_exam.r

--- R_code_exam.r
+++ R_code_exam.r
@@ -1249,6 +1249,69 @@ s1 <- stack(preds,p1)
 plot(s1, col=cl)
 
 
+# 16. R code exam Greenland monitoring 
+
+#Ice sheet melt extent
+setwd("C:/lab/Greenland/")
+library(raster)
+library(rasterVis)
+
+rlist <- list.files(pattern="melt")
+rlist
+
+import <- lapply(rlist,raster)
+Gr <- stack(import)
+levelplot(Gr)
+
+#Ice sheet difference 2007 - 1979
+rast <- raster("1979_melt.tif")
+rast2 <- raster("2007_melt.tif")
+dif <- rast2 - rast
+clb <- colorRampPalette(c("blue","white","red"))(100)
+plot(dif,col=clb,main="Melt area variation 1979 - 2007")
+dev.off()
+
+#Scatterplot cumulative annual melt area
+setwd("C:/lab/Greenland/")
+library(ggplot2)
+library(readxl)
+data <- read_excel("greenland_annual.xlsx")
+head(data)
+attach(data)
+scatter.smooth(x=Year,y=Melt_area,cex=0.7,pch=18,col="red",xlab="Year",ylab="Melt area (km2)")
+text(x=2012, y=45000000, "2012 Heat Wave",col="black", font=1.5, cex=0.7)
+dev.off()
+
+#Scatterplot annual maximum melt area
+setwd("C:/lab/Greenland/")
+library(ggplot2)
+library(readxl)
+data2 <- read_excel("greenland_annual_max.xlsx")
+head(data2)
+attach(data2)
+
+scatter.smooth(x=year,y=Max_Value,cex=0.7,pch=17,col="purple",xlab="Year",ylab="Melt area max (km2)")
+text(x=2012, y=1400000, "2012 Heat Wave",col="black", font=1.5, cex=0.7)
+dev.off()
+
+#Albedo
+#let's convert HDF file to TIFF
+setwd("C:/lab/Greenland/Albedo_Greenland/")
+library(gdalUtils)
+library(raster)
+june2000 <- get_subdatasets("MCD43C3.A2000167.006.2016190203420.hdf")
+june2000
+
+#let's choose the layer, convert it and create raster
+gdal_translate(june2000[1], dst_dataset = "15june2000_albedo.tif")
+
+rast <- brick("15june2000_albedo.tif")
+ext <- c(-0.022,-0.00,0.015,0.025)
+zoom(rast,ext=ext)
+june2000 <- crop(ext=ext)
+writeRaster(june2000,"albedojune2000.tif")
+
+
 
 
 
